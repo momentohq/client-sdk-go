fa03e1a905c9c78d7bc69d7219074be483d2aa6d
erikatharp <eem@momentohq.com>
2022-02-18 11:48:24 -0800

    chore: add documentation for cache operation responses

--- momento/cache_operation_responses.go
+++ momento/cache_operation_responses.go
@@ -1,36 +1,45 @@
 package momento
 
+// Initializes ListCacheResponse to handle list cache response.
 type ListCachesResponse struct {
 	nextToken string
 	caches    []CacheInfo
 }
 
+// Returns next token.
 func (resp *ListCachesResponse) NextToken() string {
 	return resp.nextToken
 }
 
+// Returns all caches.
 func (resp *ListCachesResponse) Caches() []CacheInfo {
 	return resp.caches
 }
 
+// Initializes CacheInfo to handle caches returned from list cache operation.
 type CacheInfo struct {
 	name string
 }
 
+// Returns cache's name.
 func (ci CacheInfo) Name() string {
 	return ci.name
 }
 
 const (
-	HIT  string = "HIT"
+	// Represents cache hit.
+	HIT string = "HIT"
+	// Represents cache miss.
 	MISS string = "MISS"
 )
 
+// Initializes GetCacheResponse to handle gRPC get response.
 type GetCacheResponse struct {
 	value  []byte
 	result string
 }
 
+// Returns value stored in cache as string if there was Hit. Returns an empty string otherwise.
 func (resp *GetCacheResponse) StringValue() string {
 	if resp.result == HIT {
 		return string(resp.value)
@@ -38,6 +47,7 @@ func (resp *GetCacheResponse) StringValue() string {
 	return ""
 }
 
+// Returns value stored in cache as bytes if there was Hit. Returns nil otherwise.
 func (resp *GetCacheResponse) ByteValue() []byte {
 	if resp.result == HIT {
 		return resp.value
@@ -45,18 +55,22 @@ func (resp *GetCacheResponse) ByteValue() []byte {
 	return nil
 }
 
+// Returns get operation result such as HIT or MISS.
 func (resp *GetCacheResponse) Result() string {
 	return resp.result
 }
 
+// Initializes SetCacheResponse to handle gRPC set response.
 type SetCacheResponse struct {
 	value []byte
 }
 
+// Decodes and returns byte value set in cache to string.
 func (resp *SetCacheResponse) StringValue() string {
 	return string(resp.value)
 }
 
+// Returns byte value set in cache.
 func (resp *SetCacheResponse) ByteValue() []byte {
 	return resp.value
 }
