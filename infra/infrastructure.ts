1e51effec4453ac9743e100187f67ad254e53cbb
erikatharp <eem@momentohq.com>
2023-01-24 21:41:33 -0800

    add secret manager to get auth token for alpha

--- infra/infrastructure.ts
+++ infra/infrastructure.ts
@@ -16,7 +16,7 @@ import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
 
 const app = new App();
 const stack = new Stack(app, 'pub-sub-demo');
-const pubSubSecret = secretsmanager.Secret.fromSecretCompleteArn(stack, "pub-sub-secret", "arn:aws:secretsmanager:us-west-2:871491162332:secret:pubsub/secret-bGqyZv");
+const pubSubSecret = secretsmanager.Secret.fromSecretNameV2(stack, "pub-sub-secret", "pubsub/secret");
 const vpc = new Vpc(stack, 'pub-sub-vpc', { maxAzs: 2 });
 const publisherCluster = new Cluster(stack, 'publisher-cluster', {vpc});
 const subscriberCluster = new Cluster(stack, 'subscriber-cluster', {vpc});
@@ -38,25 +38,25 @@ publisherTaskDefinition.addContainer("publisher-container", {
     environment: {"TEST_TOPIC_NAME": "test-topic", "TEST_NAME": "TestBasicHappyPathPublisher"},
     portMappings: [{containerPort: 3000}],
     logging: LogDriver.awsLogs({streamPrefix: 'publisher', logGroup}),
-    secrets: {"TEST_AUTH_TOKEN": Secret.fromSecretsManager(pubSubSecret)}
+    secrets: {"TEST_AUTH_TOKEN": Secret.fromSecretsManager(pubSubSecret, "AUTH_TOKEN")}
 });
 subscriberTaskDefinition.addContainer("subscriber-container", {
     containerName: 'subscriber',
     image: ContainerImage.fromAsset(resolve(__dirname, '..'), {platform: Platform.LINUX_AMD64}),
     environment: {"TEST_TOPIC_NAME": "test-topic", "TEST_NAME": "TestBasicHappyPathSubscriber"},
     logging: LogDriver.awsLogs({streamPrefix: 'subscriber', logGroup}),
-    secrets: {"TEST_AUTH_TOKEN": Secret.fromSecretsManager(pubSubSecret)}
+    secrets: {"TEST_AUTH_TOKEN": Secret.fromSecretsManager(pubSubSecret, "AUTH_TOKEN")}
 });
 
-const publisherService = new FargateService(stack, "publisher-fargate-service", {
+new FargateService(stack, "publisher-fargate-service", {
    cluster:  publisherCluster,
     taskDefinition: publisherTaskDefinition,
     desiredCount: 1
 });
-const subscriberService = new FargateService(stack, "subscriber-fargate-service", {
+new FargateService(stack, "subscriber-fargate-service", {
     cluster:  subscriberCluster,
     taskDefinition: subscriberTaskDefinition,
-    desiredCount: 1
+    desiredCount: 5
 });
 
 app.synth();
