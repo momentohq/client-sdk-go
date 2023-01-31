08d75e73e6233e26a7ab409e1e32948ab7fd555d
erikatharp <eem@momentohq.com>
2023-01-31 10:18:53 -0800

    set task count to 0

--- examples/demo/infra/infrastructure.ts
+++ examples/demo/infra/infrastructure.ts
@@ -51,12 +51,12 @@ subscriberTaskDefinition.addContainer("subscriber-container", {
 new FargateService(stack, "publisher-fargate-service", {
    cluster:  publisherCluster,
     taskDefinition: publisherTaskDefinition,
-    desiredCount: 1
+    desiredCount: 0
 });
 new FargateService(stack, "subscriber-fargate-service", {
     cluster:  subscriberCluster,
     taskDefinition: subscriberTaskDefinition,
-    desiredCount: 5
+    desiredCount: 0
 });
 
 app.synth();
