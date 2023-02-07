6c94de6c8b1695a4cd5a00fc7ff492581d5e92e2
erikatharp <eem@momentohq.com>
2023-02-07 15:30:16 -0800

    chore: accept logger factory as a param for LatestLaptop config

--- config/configurations.go
+++ config/configurations.go
@@ -14,10 +14,14 @@ const defaultMaxSessionMemoryMb = 256
 // 4 minutes.  We want to remain comfortably underneath the idle timeout for AWS NLB, which is 350s.
 const defaultMaxIdle = 4 * time.Minute
 
-func LatestLaptopConfig() *Laptop {
+func LatestLaptopConfig(loggerFactory ...logger.MomentoLoggerFactory) *Laptop {
+	defaultLoggerFactory := logger.NewNoopMomentoLoggerFactory()
+	if len(loggerFactory) != 0 {
+		defaultLoggerFactory = loggerFactory[0]
+	}
 	return &Laptop{
 		Configuration: NewSimpleCacheConfiguration(&ConfigurationProps{
-			LoggerFactory: logger.NewBuiltinMomentoLoggerFactory(),
+			LoggerFactory: defaultLoggerFactory,
 			TransportStrategy: NewStaticTransportStrategy(&TransportStrategyProps{
 				GrpcConfiguration: NewStaticGrpcConfiguration(&GrpcConfigurationProps{
 					deadline:           5 * time.Second,
@@ -33,10 +37,14 @@ type InRegion struct {
 	Configuration
 }
 
-func LatestInRegionConfig() *InRegion {
+func LatestInRegionConfig(loggerFactory ...logger.MomentoLoggerFactory) *InRegion {
+	defaultLoggerFactory := logger.NewNoopMomentoLoggerFactory()
+	if len(loggerFactory) != 0 {
+		defaultLoggerFactory = loggerFactory[0]
+	}
 	return &InRegion{
 		Configuration: NewSimpleCacheConfiguration(&ConfigurationProps{
-			LoggerFactory: logger.NewBuiltinMomentoLoggerFactory(),
+			LoggerFactory: defaultLoggerFactory,
 			TransportStrategy: NewStaticTransportStrategy(&TransportStrategyProps{
 				GrpcConfiguration: NewStaticGrpcConfiguration(&GrpcConfigurationProps{
 					deadline:           1100 * time.Millisecond,
