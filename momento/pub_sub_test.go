aa5877f2f44075400d111eec3944a90dcdc73d3d
Michael G. Schwern <schwern@pobox.com>
2023-02-13 19:15:29 -0800

    test: Restore the pub/sub test from incubating.
    The local server and client with the port will have to be thought out.
    Do we want to add a port option to the normal client?

---
+++ momento/pub_sub_test.go
@@ -0,0 +1,180 @@
+package momento
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/momentohq/client-sdk-go/auth"
+	"github.com/momentohq/client-sdk-go/config"
+)
+
+var client ScsClient
+
+func TestMain(m *testing.M) {
+	setup()
+	m.Run()
+	teardown()
+}
+
+func getClient() ScsClient {
+	credProvider, err := auth.NewEnvMomentoTokenProvider("TEST_AUTH_TOKEN")
+	if err != nil {
+		panic(err)
+	}
+	client, err := NewSimpleCacheClient(&SimpleCacheClientProps{
+		Configuration:      config.LatestLaptopConfig(),
+		CredentialProvider: credProvider,
+		DefaultTTL:         60 * time.Second,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return *client
+}
+
+func setup() {
+	ctx := context.Background()
+	client = getClient()
+	err := client.CreateCache(ctx, &CreateCacheRequest{
+		CacheName: "test-cache",
+	})
+	if err != nil {
+		var momentoErr MomentoError
+		if errors.As(err, &momentoErr) {
+			if momentoErr.Code() != AlreadyExistsError {
+				panic(err)
+			}
+		}
+	}
+}
+
+func teardown() {
+	client.Close()
+}
+
+func publishTopic(ctx context.Context, pubClient ScsClient, i int) {
+	var topicVal TopicValue
+
+	if i%2 == 0 {
+		topicVal = &TopicValueString{Text: "hello txt"}
+	} else {
+		topicVal = &TopicValueBytes{Bytes: []byte("hello bytes")}
+	}
+
+	_, err := pubClient.TopicPublish(ctx, &TopicPublishRequest{
+		CacheName: "test-cache",
+		TopicName: "test-topic",
+		Value:     topicVal,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Basic happy path test using a context which we cancel
+func TestHappyPathPubSub(t *testing.T) {
+	ctx := context.Background()
+	cancelContext, cancelFunction := context.WithCancel(ctx)
+
+	sub, err := client.TopicSubscribe(ctx, &TopicSubscribeRequest{
+		CacheName: "test-cache",
+		TopicName: "test-topic",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	numMessagesToSend := 10
+	numMessagesReceived := 0
+	go func() {
+		for {
+			select {
+			case <-cancelContext.Done():
+				return
+			default:
+				_, err := sub.Item()
+				if err != nil {
+					panic(err)
+				}
+				numMessagesReceived++
+			}
+		}
+	}()
+	time.Sleep(time.Second)
+
+	for i := 0; i < numMessagesToSend; i++ {
+		publishTopic(ctx, client, i)
+		time.Sleep(time.Second)
+	}
+	cancelFunction()
+
+	if numMessagesReceived != numMessagesToSend {
+		t.Errorf("expected %d messages but received %d", numMessagesToSend, numMessagesReceived)
+	}
+}
+
+// Basic happy path test using local test server
+// TODO: are we going to keep the local client and server around?
+// func TestBasicHappyPathLocalPubSub(t *testing.T) {
+// 	ctx := context.Background()
+// 	testPortToUse := 3000
+// 	go func() {
+// 		newMomentoLocalTestServer(testPortToUse)
+// 	}()
+
+// 	localClient, err := newLocalScsClient(testPortToUse)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	sub, err := localClient.SubscribeTopic(ctx, &TopicSubscribeRequest{
+// 		CacheName: "test-cache",
+// 		TopicName: "test-topic",
+// 	})
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	numMessagesReceived := 0
+// 	numMessagesToSend := 10
+// 	go func() {
+// 		for {
+// 			_, err := sub.Item()
+// 			if err != nil {
+// 				panic(err)
+// 			}
+// 			numMessagesReceived++
+// 			if numMessagesToSend == numMessagesReceived {
+// 				return
+// 			}
+// 		}
+// 	}()
+
+// 	for i := 0; i < numMessagesToSend; i++ {
+// 		var topicVal TopicValue
+// 		if i%2 == 0 {
+// 			topicVal = &TopicValueString{
+// 				Text: fmt.Sprintf("string hello %d", i),
+// 			}
+// 		} else {
+// 			topicVal = &TopicValueBytes{
+// 				Bytes: []byte(fmt.Sprintf("byte hello %d", i)),
+// 			}
+// 		}
+// 		err := localClient.PublishTopic(ctx, &TopicPublishRequest{
+// 			CacheName: "test-cache",
+// 			TopicName: "test-topic",
+// 			Value:     topicVal,
+// 		})
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		time.Sleep(time.Second)
+// 	}
+
+// 	if numMessagesToSend != numMessagesReceived {
+// 		t.Errorf("expected %d messages but got %d", numMessagesToSend, numMessagesReceived)
+// 	}
+// }
