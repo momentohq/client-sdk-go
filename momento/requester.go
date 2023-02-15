69b2ffb0899501c9ebc978dedc7cf164153b7140
Michael G. Schwern <schwern@pobox.com>
2023-02-15 11:22:51 -0800

    refactor: common.go -> requester.co (#128)
    Make it clear the file is all about the requester interface, not a jumble of
    common functions. Document it for internal use.

--- momento/requester.go
+++ momento/requester.go
@@ -1,5 +1,10 @@
 package momento
 
+// The requester interface is implemented by individual
+// method request objects, for example SetRequest.
+// requester.template is a template file to help implement
+// a requester.
+
 import (
 	"context"
 	"errors"
