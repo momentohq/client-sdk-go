08f760f97a4e0d075b251abdf097dec7a3ed1627
Michael G. Schwern <schwern@pobox.com>
2023-02-16 15:49:20 -0800

    Values are allowed to be blank.
    Complete the conversion of the test suite to Ginkgo.

--- momento/scalar_test.go
+++ momento/scalar_test.go
@@ -2,16 +2,31 @@ package momento_test
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
 
 	"github.com/momentohq/client-sdk-go/auth"
 	"github.com/momentohq/client-sdk-go/config"
 	. "github.com/momentohq/client-sdk-go/momento"
 )
+
+func HaveMomentoErrorCode(code string) types.GomegaMatcher {
+	return WithTransform(
+		func(err error) (string, error) {
+			switch mErr := err.(type) {
+			case MomentoError:
+				return mErr.Code(), nil
+			default:
+				return "", fmt.Errorf("Expected MomentoError, but got %T", err)
+			}
+		}, Equal(code),
+	)
+}
 
 var _ = Describe("Scalar methods", func() {
 	var clientProps SimpleCacheClientProps
@@ -44,14 +59,14 @@ var _ = Describe("Scalar methods", func() {
 		testCacheName = uuid.NewString()
 		Expect(
 			client.CreateCache(ctx, &CreateCacheRequest{CacheName: testCacheName}),
-		).To(Succeed())
+		).To(BeAssignableToTypeOf(&CreateCacheSuccess{}))
 		DeferCleanup(func() {
 			client.DeleteCache(ctx, &DeleteCacheRequest{CacheName: testCacheName})
 		})
 	})
 
 	DescribeTable(`Gets, Sets, and Deletes`,
-		func(key Value, value Value, expectedString string, expectedBytes []byte) {
+		func(key Key, value Value, expectedString string, expectedBytes []byte) {
 			Expect(
 				client.Set(ctx, &SetRequest{
 					CacheName: testCacheName,
@@ -90,6 +105,65 @@ var _ = Describe("Scalar methods", func() {
 		},
 		Entry("when the key and value are strings", String("key"), String("value"), "value", []byte("value")),
 		Entry("when the key and value are bytes", Bytes([]byte{1, 2, 3}), Bytes([]byte("string")), "string", []byte("string")),
+		Entry("when the value is empty", String("key"), String(""), "", []byte("")),
+		Entry("when the value is blank", String("key"), String("  "), "  ", []byte("  ")),
+	)
+
+	It(`errors when the cache is missing`, func() {
+		cacheName := uuid.NewString()
+		key := String("key")
+		value := String("value")
+
+		getResp, err := client.Get(ctx, &GetRequest{
+			CacheName: cacheName,
+			Key:       key,
+		})
+		Expect(getResp).To(BeNil())
+		Expect(err).To(HaveMomentoErrorCode(NotFoundError))
+
+		setResp, err := client.Set(ctx, &SetRequest{
+			CacheName: cacheName,
+			Key:       key,
+			Value:     value,
+		})
+		Expect(setResp).To(BeNil())
+		Expect(err).To(HaveMomentoErrorCode(NotFoundError))
+
+		deleteResp, err := client.Delete(ctx, &DeleteRequest{
+			CacheName: cacheName,
+			Key:       key,
+		})
+		Expect(deleteResp).To(BeNil())
+		Expect(err).To(HaveMomentoErrorCode(NotFoundError))
+	})
+
+	DescribeTable(`invalid cache names and keys`,
+		func(cacheName string, key Key, value Key) {
+			getResp, err := client.Get(ctx, &GetRequest{
+				CacheName: cacheName,
+				Key:       key,
+			})
+			Expect(getResp).To(BeNil())
+			Expect(err).To(HaveMomentoErrorCode(InvalidArgumentError))
+
+			setResp, err := client.Set(ctx, &SetRequest{
+				CacheName: cacheName,
+				Key:       key,
+				Value:     value,
+			})
+			Expect(setResp).To(BeNil())
+			Expect(err).To(HaveMomentoErrorCode(InvalidArgumentError))
+
+			deleteResp, err := client.Delete(ctx, &DeleteRequest{
+				CacheName: cacheName,
+				Key:       key,
+			})
+			Expect(deleteResp).To(BeNil())
+			Expect(err).To(HaveMomentoErrorCode(InvalidArgumentError))
+		},
+		Entry(`With an empty cache name`, "", String("key"), String("value")),
+		Entry(`With an bank cache name`, "   ", String("key"), String("value")),
+		Entry(`With an empty key`, cacheName(), String(""), String("value")),
 	)
 
 	Describe(`Set`, func() {
