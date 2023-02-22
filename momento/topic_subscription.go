ae914599cb70c45d7f61bd763767130da41a6f8d
eaddingtonwhite <5491827+eaddingtonwhite@users.noreply.github.com>
2023-02-21 17:54:51 -0800

    feat: handle h2 go-away from server for topic stream (#151)
    Closes #74

--- momento/topic_subscription.go
+++ momento/topic_subscription.go
@@ -1,57 +1,96 @@
 package momento
 
 import (
+	"context"
 	"io"
 
+	"github.com/momentohq/client-sdk-go/internal/momentoerrors"
 	pb "github.com/momentohq/client-sdk-go/internal/protos"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type TopicSubscription interface {
-	Item() (TopicValue, error)
+	Item(ctx context.Context) (TopicValue, error)
 }
 
 type topicSubscription struct {
-	grpcClient grpc.ClientStream
+	grpcClient         grpc.ClientStream
+	momentoTopicClient *pubSubClient
+	cacheName          string
+	topicName          string
 }
 
-func (s topicSubscription) Item() (TopicValue, error) {
-	retryCount := 0
-	maxRetries := 2
+func (s *topicSubscription) Item(ctx context.Context) (TopicValue, error) {
 	for {
 		rawMsg := new(pb.XSubscriptionItem)
 		if err := s.grpcClient.RecvMsg(rawMsg); err != nil {
-			if err == io.EOF {
-				// TODO think about retry and re-establish more
-				return nil, nil
+			err := s.handleStreamError(ctx, err)
+			if err != nil {
+				return nil, err
 			}
-			return nil, err
-		}
-		switch typedMsg := rawMsg.Kind.(type) {
-		case *pb.XSubscriptionItem_Discontinuity:
-			retryCount++
-			if retryCount > maxRetries {
-				return nil, nil
+		} else {
+			switch typedMsg := rawMsg.Kind.(type) {
+			case *pb.XSubscriptionItem_Discontinuity:
+				continue
+			case *pb.XSubscriptionItem_Item:
+				switch subscriptionItem := typedMsg.Item.Value.Kind.(type) {
+				case *pb.XTopicValue_Text:
+					return &TopicValueString{
+						Text: subscriptionItem.Text,
+					}, nil
+				case *pb.XTopicValue_Binary:
+					return &TopicValueBytes{
+						Bytes: subscriptionItem.Binary,
+					}, nil
+				}
+			case *pb.XSubscriptionItem_Heartbeat:
+				// FIXME add warning logging here
+				continue
+			default:
+				// FIXME add warning logging here
+				// Ignore unknown responses, so we don't stop polling if we add a new message.
+				// For example, we wouldn't want to stop because of an unknown heartbeat response.
+				continue
 			}
-			continue
-		case *pb.XSubscriptionItem_Item:
-			switch subscriptionItem := typedMsg.Item.Value.Kind.(type) {
-			case *pb.XTopicValue_Text:
-				return &TopicValueString{
-					Text: subscriptionItem.Text,
-				}, nil
-			case *pb.XTopicValue_Binary:
-				return &TopicValueBytes{
-					Bytes: subscriptionItem.Binary,
-				}, nil
-			}
-		case *pb.XSubscriptionItem_Heartbeat:
-			// Doesn't count against our retries.
-			continue
-		default:
-			// Ignore unknown responses so we don't stop polling if we add a new message.
-			// For example, we wouldn't want to stop because of an unknown heartbeat response.
-			continue
 		}
 	}
+}
+
+func (s *topicSubscription) handleStreamError(ctx context.Context, err error) error {
+	var returnErr error
+	if err == io.EOF {
+		returnErr = s.reInitStream(ctx)
+	} else if grpcStatusErr, ok := status.FromError(err); ok {
+		// FIXME ideally we could retry on raw h2 error not grpc error
+		// See this ticket for follow up https://github.com/momentohq/client-sdk-go/issues/156
+		if grpcStatusErr.Code() == codes.Internal &&
+			grpcStatusErr.Message() == "stream terminated by RST_STREAM with error code: NO_ERROR" {
+
+			returnErr = s.reInitStream(ctx)
+
+		}
+	}
+	if returnErr != nil {
+		return momentoerrors.NewMomentoSvcErr(
+			momentoerrors.InternalServerError,
+			"Fatal error getting item from topic",
+			err,
+		)
+	}
+	return nil
+}
+
+func (s *topicSubscription) reInitStream(ctx context.Context) error {
+	newStream, err := s.momentoTopicClient.TopicSubscribe(ctx, &TopicSubscribeRequest{
+		CacheName: s.cacheName,
+		TopicName: s.topicName,
+	})
+	if err != nil {
+		return err
+	}
+	s.grpcClient = newStream
+	return nil
 }
