aab2c4f2f0ed1e4de6eeea360b8d3110e096399d
pgautier404 <pgautier404@users.noreply.github.com>
2023-02-27 17:49:03 -0800

    chore: add list testing (#202)
    * chore: add list testing
    
    * chore: list testing continued
    
    * finishing first pass
    
    * fix: PR feedback

--- momento/momento_suite_test.go
+++ momento/momento_suite_test.go
@@ -22,7 +22,7 @@ func HaveMomentoErrorCode(code string) types.GomegaMatcher {
 			case momento.MomentoError:
 				return mErr.Code(), nil
 			default:
-				return "", fmt.Errorf("Expected MomentoError, but got %T", err)
+				return "", fmt.Errorf("expected MomentoError, but got %T", err)
 			}
 		}, Equal(code),
 	)
@@ -36,6 +36,19 @@ func HaveSetLength(length int) types.GomegaMatcher {
 				return len(rtype.ValueString()), nil
 			default:
 				return 0, fmt.Errorf("expected set fetch hit but got %T", fetchResp)
+			}
+		}, Equal(length),
+	)
+}
+
+func HaveListLength(length int) types.GomegaMatcher {
+	return WithTransform(
+		func(fetchResp momento.ListFetchResponse) (int, error) {
+			switch rtype := fetchResp.(type) {
+			case *momento.ListFetchHit:
+				return len(rtype.ValueList()), nil
+			default:
+				return 0, fmt.Errorf("expected list fetch hit but got %T", fetchResp)
 			}
 		}, Equal(length),
 	)
