c5f3580759cdd5c6ce92090f078750f3b16e488e
eaddingtonwhite <5491827+eaddingtonwhite@users.noreply.github.com>
2023-03-01 15:15:33 -0800

    feat: removes maxIdle and maxSessionMem from client config for now (#216)

--- config/transport_strategy.go
+++ config/transport_strategy.go
@@ -5,14 +5,6 @@ import "time"
 type TransportStrategyProps struct {
 	// low-level gRPC settings for communication with the Momento server
 	GrpcConfiguration GrpcConfiguration
-
-	// The maximum duration for which a connection may remain idle before being replaced.  This
-	// setting can be used to force re-connection of a client if it has been idle for too long.
-	// In environments such as AWS lambda, if the lambda is suspended for too long the connection
-	// may be closed by the load balancer, resulting in an error on the subsequent request.  If
-	// this setting is set to a duration less than the load balancer timeout, we can ensure that
-	// the connection will be refreshed to avoid errors.
-	MaxIdle time.Duration
 }
 
 type TransportStrategy interface {
@@ -31,21 +23,15 @@ type TransportStrategy interface {
 	// WithClientTimeout Copy constructor for overriding the client sie timeout. Returns a new
 	// TransportStrategy with the specified client side timeout.
 	WithClientTimeout(clientTimeout time.Duration) TransportStrategy
-
-	// WithMaxIdle Copy constructor for overriding the max idle connection timeout. Returns a new
-	// TransportStrategy with the specified client side idle connection timeout.
-	WithMaxIdle(maxIdle time.Duration) TransportStrategy
 }
 
 type StaticGrpcConfiguration struct {
-	deadline           time.Duration
-	maxSessionMemoryMb uint32
+	deadline time.Duration
 }
 
 func NewStaticGrpcConfiguration(grpcConfiguration *GrpcConfigurationProps) *StaticGrpcConfiguration {
 	return &StaticGrpcConfiguration{
-		deadline:           grpcConfiguration.deadline,
-		maxSessionMemoryMb: grpcConfiguration.maxSessionMemoryMb,
+		deadline: grpcConfiguration.deadline,
 	}
 }
 
@@ -53,21 +39,9 @@ func (s *StaticGrpcConfiguration) GetDeadline() time.Duration {
 	return s.deadline
 }
 
-func (s *StaticGrpcConfiguration) GetMaxSessionMemoryMb() uint32 {
-	return s.maxSessionMemoryMb
-}
-
-func (s *StaticGrpcConfiguration) WithMaxSessionMb(maxSessionMemoryMb uint32) GrpcConfiguration {
-	return &StaticGrpcConfiguration{
-		deadline:           s.deadline,
-		maxSessionMemoryMb: maxSessionMemoryMb,
-	}
-}
-
 func (s *StaticGrpcConfiguration) WithDeadline(deadline time.Duration) GrpcConfiguration {
 	return &StaticGrpcConfiguration{
-		deadline:           deadline,
-		maxSessionMemoryMb: s.maxSessionMemoryMb,
+		deadline: deadline,
 	}
 }
 
@@ -90,7 +64,6 @@ func (s *StaticTransportStrategy) WithClientTimeout(clientTimeout time.Duration)
 func NewStaticTransportStrategy(props *TransportStrategyProps) TransportStrategy {
 	return &StaticTransportStrategy{
 		grpcConfig: props.GrpcConfiguration,
-		maxIdle:    props.MaxIdle,
 	}
 }
 
