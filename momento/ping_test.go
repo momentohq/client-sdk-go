0421d516e201836fda4968bc64b87979111ab908
pgautier404 <pgautier404@users.noreply.github.com>
2023-03-08 16:25:24 -0800

    feat: add ping (#249)
    * feat: adding ping
    
    * add testing
    
    * respect client timeout for pings
    
    * correct error message

---
+++ momento/ping_test.go
@@ -0,0 +1,29 @@
+package momento_test
+
+import (
+	. "github.com/momentohq/client-sdk-go/momento/test_helpers"
+	. "github.com/momentohq/client-sdk-go/responses"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ping", func() {
+	var sharedContext SharedContext
+
+	BeforeEach(func() {
+		sharedContext = NewSharedContext()
+		sharedContext.CreateDefaultCache()
+		DeferCleanup(func() {
+			sharedContext.Close()
+		})
+	})
+
+	It("receives successful ping responses", func() {
+		for i := 0; i < 25; i++ {
+			Expect(
+				sharedContext.Client.Ping(sharedContext.Ctx),
+			).To(BeAssignableToTypeOf(&PingSuccess{}))
+		}
+	})
+
+})
