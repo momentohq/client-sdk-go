2eaa37a393976028cea21a30b4559ab2447a8af1
Pete Gautier <pete@momentohq.com>
2023-03-10 15:20:47 -0800

    remove unneeded stuff from request

--- momento/sorted_set_get_score.go
+++ momento/sorted_set_get_score.go
@@ -1,16 +1,7 @@
 package momento
 
-import (
-	pb "github.com/momentohq/client-sdk-go/internal/protos"
-	"github.com/momentohq/client-sdk-go/responses"
-)
-
 type SortedSetGetScoreRequest struct {
 	CacheName string
 	SetName   string
 	Value     Value
-
-	grpcRequest  *pb.XSortedSetGetScoreRequest
-	grpcResponse *pb.XSortedSetGetScoreResponse
-	response     responses.SortedSetGetScoresResponse
 }
