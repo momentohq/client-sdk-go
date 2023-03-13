50b695b06f89e74ed11e4a9df857785ecba099cb
pgautier404 <pgautier404@users.noreply.github.com>
2023-03-13 09:11:52 -0700

    fix: simplify abstractions involved in sorted set get score (#276)
    * fix: simplify abstractions involved in sorted set get elements
    
    * fix: fix accessor name

--- responses/sorted_set_get_score.go
+++ responses/sorted_set_get_score.go
@@ -10,19 +10,24 @@ type SortedSetGetScoreMiss struct{}
 
 func (SortedSetGetScoreMiss) isSortedSetGetScoreResponse() {}
 
+// SortedSetGetScoreInvalid Invalid Response to a cache SortedSetGetScore api request.
+type SortedSetGetScoreInvalid struct{}
+
+func (SortedSetGetScoreInvalid) isSortedSetGetScoreResponse() {}
+
 // SortedSetGetScoreHit Hit Response to a cache SortedSetGetScore api request.
 type SortedSetGetScoreHit struct {
-	score SortedSetGetScore
+	score float64
 }
 
 func (SortedSetGetScoreHit) isSortedSetGetScoreResponse() {}
 
 // NewSortedSetGetScoreHit returns a new SortedSetGetScoreHit containing the supplied score.
-func NewSortedSetGetScoreHit(score SortedSetGetScore) *SortedSetGetScoreHit {
+func NewSortedSetGetScoreHit(score float64) *SortedSetGetScoreHit {
 	return &SortedSetGetScoreHit{score: score}
 }
 
-// Score returns a SortedSetGetScore.
-func (r SortedSetGetScoreHit) Score() SortedSetGetScore {
+// Score returns the float64 score value.
+func (r SortedSetGetScoreHit) Score() float64 {
 	return r.score
 }
