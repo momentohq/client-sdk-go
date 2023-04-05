065d880ec77e6d46a191b58bb1b6fa1373f8d54b
Pete Gautier <pete@momentohq.com>
2023-04-05 13:09:04 -0700

    fix: improve test

--- batchutils/batch_operations_test.go
+++ batchutils/batch_operations_test.go
@@ -9,7 +9,9 @@ import (
 	"github.com/momentohq/client-sdk-go/auth"
 	"github.com/momentohq/client-sdk-go/batchutils"
 	"github.com/momentohq/client-sdk-go/config"
+	"github.com/momentohq/client-sdk-go/config/logger"
 	. "github.com/momentohq/client-sdk-go/momento"
+	"github.com/momentohq/client-sdk-go/responses"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -30,7 +32,11 @@ var _ = Describe("Batch operations", func() {
 		if err != nil {
 			panic(err)
 		}
-		client, err = NewCacheClient(config.LaptopLatest(), credentialProvider, time.Second*60)
+		client, err = NewCacheClient(
+			config.LaptopLatestWithLogger(logger.NewNoopMomentoLoggerFactory()),
+			credentialProvider,
+			time.Second*60,
+		)
 		if err != nil {
 			panic(err)
 		}
@@ -54,12 +60,29 @@ var _ = Describe("Batch operations", func() {
 		}
 	})
 
-	It("batch deletes with NewCacheClient", func() {
+	It("batch deletes", func() {
 		errors := batchutils.BatchDelete(ctx, &batchutils.BatchDeleteRequest{
 			Client:    client,
 			CacheName: cacheName,
-			Keys:      keys[5:20],
+			Keys:      keys[5:21],
 		})
 		Expect(len(errors)).To(Equal(0))
+		for i := 0; i < 50; i++ {
+			resp, err := client.Get(ctx, &GetRequest{
+				CacheName: cacheName,
+				Key:       keys[i],
+			})
+			Expect(err).To(BeNil())
+			switch resp.(type) {
+			case *responses.GetHit:
+				if i >= 5 && i <= 20 {
+					Fail("got a hit for #%d that should be a miss", i)
+				}
+			case *responses.GetMiss:
+				if !(i >= 5 && i <= 20) {
+					Fail("got a miss for #%d that should be a hit", i)
+				}
+			}
+		}
 	})
 })
