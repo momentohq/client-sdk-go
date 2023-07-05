0c03c626db29e1435795ba834e4d4eceb42a09b9
Pratik Agarwal <pratik@momentohq.com>
2023-07-04 18:06:37 -0700

    feat: add request and responses for sorted-set-length and sorted-set-length-by-score

---
+++ responses/sorted_set_length_by_score.go
@@ -0,0 +1,28 @@
+package responses
+
+// SortedSetLengthByScoreResponse is the base response type for a sorted set length request.
+type SortedSetLengthByScoreResponse interface {
+	isSortedSetLengthByScoreResponse()
+}
+
+// SortedSetLengthByScoreHit indicates a sorted set length request was a hit.
+type SortedSetLengthByScoreHit struct {
+	value uint32
+}
+
+func (SortedSetLengthByScoreHit) isSortedSetLengthByScoreResponse() {}
+
+// Length returns the length of the sorted set.
+func (resp SortedSetLengthByScoreHit) Length() uint32 {
+	return resp.value
+}
+
+// SortedSetLengthByScoreMiss indicates a sorted set length request was a miss.
+type SortedSetLengthByScoreMiss struct{}
+
+func (SortedSetLengthByScoreMiss) isSortedSetLengthByScoreResponse() {}
+
+// NewSortedSetLengthByScoreHit returns a new SortedSetLengthByScoreHit containing the supplied value.
+func NewSortedSetLengthByScoreHit(value uint32) *SortedSetLengthByScoreHit {
+	return &SortedSetLengthByScoreHit{value: value}
+}
