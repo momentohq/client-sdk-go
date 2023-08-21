d184d195c3ba48669b5967599bb974c6274631d4
anitarua <anita@momentohq.com>
2023-08-21 12:16:34 -0700

    lambda simple get example typescript cdk stack

---
+++ examples/lambda-examples/simple-get/infrastructure/lib/simple-get-stack.ts
@@ -0,0 +1,36 @@
+import * as path from 'path';
+import * as cdk from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as secrets from 'aws-cdk-lib/aws-secretsmanager';
+import * as go from '@aws-cdk/aws-lambda-go-alpha';
+
+export class SimpleGetStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const momentoAuthTokenParam = new cdk.CfnParameter(this, 'MomentoAuthToken', {
+      type: 'String',
+      description: 'The Momento Auth Token that will be used to read from the cache.',
+      noEcho: true,
+    });
+
+    const authTokenSecret = new secrets.Secret(this, 'MomentoSimpleGetAuthToken', {
+      secretName: 'MomentoSimpleGetAuthToken',
+      secretStringValue: new cdk.SecretValue(momentoAuthTokenParam.valueAsString),
+    });
+
+    const getLambda = new go.GoFunction(this, 'MomentoSimpleGet', {
+      functionName: 'MomentoSimpleGet',
+      runtime: lambda.Runtime.GO_1_X,
+      entry: path.join(__dirname, '../../lambda'),
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 128,
+      environment: {
+        MOMENTO_AUTH_TOKEN_SECRET_NAME: authTokenSecret.secretName,
+      },
+    });
+
+    authTokenSecret.grantRead(getLambda);
+  }
+}
