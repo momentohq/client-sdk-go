c79d1b0f1aaac35970b36d8b33abbac3690615f0
Pratik Agarwal <pratik@momentohq.com>
2023-08-30 16:06:39 -0300

    chore: cleanup

--- momento/list_test.go
+++ momento/list_test.go
@@ -1025,12 +1025,10 @@ var _ = Describe("List methods", func() {
 					}
 
 					startIndex := int32(-2)
-					endIndex := int32(-1)
 					fetchResp, err := client.ListFetch(sharedContext.Ctx, &ListFetchRequest{
 						CacheName:  cacheName,
 						ListName:   sharedContext.CollectionName,
 						StartIndex: &startIndex,
-						EndIndex:   &endIndex,
 					})
 
 					Expect(err).To(BeNil())
