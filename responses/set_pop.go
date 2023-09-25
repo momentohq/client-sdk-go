f7cbaa4e20ef7f9ae1c4f7c8d3395d6e938ae48c
anitarua <anita@momentohq.com>
2023-09-25 13:32:24 -0700

    feat: add SetPop

---
+++ responses/set_pop.go
@@ -0,0 +1,41 @@
+package responses
+
+// SetPopResponse is the base response type for a set fetch request.
+type SetPopResponse interface {
+	isSetPopResponse()
+}
+
+// SetPopHit indicates a set fetch request was a hit.
+type SetPopHit struct {
+	elements       [][]byte
+	elementsString []string
+}
+
+func (SetPopHit) isSetPopResponse() {}
+
+// ValueString returns the data as a Set whose values are utf-8 strings, decoded from the underlying byte arrays.
+func (resp SetPopHit) ValueString() []string {
+	if resp.elementsString == nil {
+		for _, value := range resp.elements {
+			resp.elementsString = append(resp.elementsString, string(value))
+		}
+	}
+	return resp.elementsString
+}
+
+// ValueByte returns the data as a Set whose values are byte arrays.
+func (resp SetPopHit) ValueByte() [][]byte {
+	return resp.elements
+}
+
+// SetPopMiss indicates a set fetch request was a miss.
+type SetPopMiss struct{}
+
+func (SetPopMiss) isSetPopResponse() {}
+
+// NewSetPopHit returns a new SetPopHit containing the supplied elements.
+func NewSetPopHit(elements [][]byte) *SetPopHit {
+	return &SetPopHit{
+		elements: elements,
+	}
+}
