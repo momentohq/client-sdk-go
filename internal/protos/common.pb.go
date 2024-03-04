d2e4e938c040cdbf15c099d84df34592a7cf37bc
Pete Gautier <pete@momentohq.com>
2024-03-04 11:47:44 -0600

    chore: recompiled protos after fixing messages

--- internal/protos/common.pb.go
+++ internal/protos/common.pb.go
@@ -285,6 +285,82 @@ func (x *NotEqual) GetValueToCheck() []byte {
 	return nil
 }
 
+type XUnbounded struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *XUnbounded) Reset() {
+	*x = XUnbounded{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_common_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *XUnbounded) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*XUnbounded) ProtoMessage() {}
+
+func (x *XUnbounded) ProtoReflect() protoreflect.Message {
+	mi := &file_common_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use XUnbounded.ProtoReflect.Descriptor instead.
+func (*XUnbounded) Descriptor() ([]byte, []int) {
+	return file_common_proto_rawDescGZIP(), []int{6}
+}
+
+type XEmpty struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *XEmpty) Reset() {
+	*x = XEmpty{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_common_proto_msgTypes[7]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *XEmpty) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*XEmpty) ProtoMessage() {}
+
+func (x *XEmpty) ProtoReflect() protoreflect.Message {
+	mi := &file_common_proto_msgTypes[7]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use XEmpty.ProtoReflect.Descriptor instead.
+func (*XEmpty) Descriptor() ([]byte, []int) {
+	return file_common_proto_rawDescGZIP(), []int{7}
+}
+
 var File_common_proto protoreflect.FileDescriptor
 
 var file_common_proto_rawDesc = []byte{
@@ -303,11 +379,15 @@ var file_common_proto_rawDesc = []byte{
 	0x0c, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x54, 0x6f, 0x43, 0x68, 0x65, 0x63, 0x6b, 0x22, 0x30, 0x0a,
 	0x08, 0x4e, 0x6f, 0x74, 0x45, 0x71, 0x75, 0x61, 0x6c, 0x12, 0x24, 0x0a, 0x0e, 0x76, 0x61, 0x6c,
 	0x75, 0x65, 0x5f, 0x74, 0x6f, 0x5f, 0x63, 0x68, 0x65, 0x63, 0x6b, 0x18, 0x01, 0x20, 0x01, 0x28,
-	0x0c, 0x52, 0x0c, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x54, 0x6f, 0x43, 0x68, 0x65, 0x63, 0x6b, 0x42,
-	0x32, 0x5a, 0x30, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6d, 0x6f,
-	0x6d, 0x65, 0x6e, 0x74, 0x6f, 0x68, 0x71, 0x2f, 0x63, 0x6c, 0x69, 0x65, 0x6e, 0x74, 0x2d, 0x73,
-	0x64, 0x6b, 0x2d, 0x67, 0x6f, 0x3b, 0x63, 0x6c, 0x69, 0x65, 0x6e, 0x74, 0x5f, 0x73, 0x64, 0x6b,
-	0x5f, 0x67, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x0c, 0x52, 0x0c, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x54, 0x6f, 0x43, 0x68, 0x65, 0x63, 0x6b, 0x22,
+	0x0c, 0x0a, 0x0a, 0x5f, 0x55, 0x6e, 0x62, 0x6f, 0x75, 0x6e, 0x64, 0x65, 0x64, 0x22, 0x08, 0x0a,
+	0x06, 0x5f, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x42, 0x59, 0x0a, 0x0b, 0x67, 0x72, 0x70, 0x63, 0x2e,
+	0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x50, 0x01, 0x5a, 0x30, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62,
+	0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6d, 0x6f, 0x6d, 0x65, 0x6e, 0x74, 0x6f, 0x68, 0x71, 0x2f, 0x63,
+	0x6c, 0x69, 0x65, 0x6e, 0x74, 0x2d, 0x73, 0x64, 0x6b, 0x2d, 0x67, 0x6f, 0x3b, 0x63, 0x6c, 0x69,
+	0x65, 0x6e, 0x74, 0x5f, 0x73, 0x64, 0x6b, 0x5f, 0x67, 0x6f, 0xaa, 0x02, 0x15, 0x4d, 0x6f, 0x6d,
+	0x65, 0x6e, 0x74, 0x6f, 0x2e, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e, 0x43, 0x6f, 0x6d, 0x6d,
+	0x6f, 0x6e, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -322,7 +402,7 @@ func file_common_proto_rawDescGZIP() []byte {
 	return file_common_proto_rawDescData
 }
 
-var file_common_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_common_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_common_proto_goTypes = []interface{}{
 	(*Present)(nil),            // 0: common.Present
 	(*PresentAndNotEqual)(nil), // 1: common.PresentAndNotEqual
@@ -330,6 +410,8 @@ var file_common_proto_goTypes = []interface{}{
 	(*Equal)(nil),              // 3: common.Equal
 	(*AbsentOrEqual)(nil),      // 4: common.AbsentOrEqual
 	(*NotEqual)(nil),           // 5: common.NotEqual
+	(*XUnbounded)(nil),         // 6: common._Unbounded
+	(*XEmpty)(nil),             // 7: common._Empty
 }
 var file_common_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -417,6 +499,30 @@ func file_common_proto_init() {
 				return nil
 			}
 		}
+		file_common_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*XUnbounded); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_common_proto_msgTypes[7].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*XEmpty); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -424,7 +530,7 @@ func file_common_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_common_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
