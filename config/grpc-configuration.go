9e5afb11bae539f4b2166b7d1c926428da46b830
anitarua <anita@momentohq.com>
2024-03-05 12:33:32 -0800

    fix lint error

--- config/grpc-configuration.go
+++ config/grpc-configuration.go
@@ -7,34 +7,6 @@ type GrpcConfigurationProps struct {
 	// with a DeadlineExceeded error.
 	deadline time.Duration
 
-	// Indicates if it permissible to send keepalive pings from the client without any outstanding streams.
-	//
-	// NOTE: keep-alives are very important for long-lived server environments where there may be periods of time
-	// when the connection is idle. However, they are very problematic for lambda environments where the lambda
-	// runtime is continuously frozen and unfrozen, because the lambda may be frozen before the "ACK" is received
-	// from the server. This can cause the keep-alive to timeout even though the connection is completely healthy.
-	// Therefore, keep-alives should be disabled in lambda and similar environments.
-	keepAlivePermitWithoutCalls bool
-
-	// After waiting for a duration of this time, if the keepalive ping sender does not receive the ping ack,
-	// it will close the transport.
-	//
-	// NOTE: keep-alives are very important for long-lived server environments where there may be periods of time
-	// when the connection is idle. However, they are very problematic for lambda environments where the lambda
-	// runtime is continuously frozen and unfrozen, because the lambda may be frozen before the "ACK" is received
-	// from the server. This can cause the keep-alive to timeout even though the connection is completely healthy.
-	// Therefore, keep-alives should be disabled in lambda and similar environments.
-	keepAliveTimeout time.Duration
-
-	// After a duration of this time the client/server pings its peer to see if the transport is still alive.
-	//
-	// NOTE: keep-alives are very important for long-lived server environments where there may be periods of time
-	// when the connection is idle. However, they are very problematic for lambda environments where the lambda
-	// runtime is continuously frozen and unfrozen, because the lambda may be frozen before the "ACK" is received
-	// from the server. This can cause the keep-alive to timeout even though the connection is completely healthy.
-	// Therefore, keep-alives should be disabled in lambda and similar environments.
-	keepAliveTime time.Duration
-
 	// The maximum message length the client can send to the server.  If the client attempts to send a message
 	// larger than this size, it will result in a RESOURCE_EXHAUSTED error.
 	maxSendMessageLength int
@@ -55,39 +27,42 @@ type GrpcConfiguration interface {
 	WithDeadline(deadline time.Duration) GrpcConfiguration
 
 	// Returns bool indicating if it is permissible to send keepalive pings from the client without any outstanding calls.
-	//
-	// NOTE: keep-alives are very important for long-lived server environments where there may be periods of time
-	// when the connection is idle. However, they are very problematic for lambda environments where the lambda
-	// runtime is continuously frozen and unfrozen, because the lambda may be frozen before the "ACK" is received
-	// from the server. This can cause the keep-alive to timeout even though the connection is completely healthy.
-	// Therefore, keep-alives should be disabled in lambda and similar environments.
 	GetKeepAlivePermitWithoutCalls() bool
 
 	// WithKeepAlivePermitWithoutCalls Copy constructor for overriding the keepalive permit without calls.
+	// Indicates if it permissible to send keepalive pings from the client without any outstanding streams.
+	//
+	// NOTE: keep-alives are very important for long-lived server environments where there may be periods of time
+	// when the connection is idle. However, they are very problematic for lambda environments where the lambda
+	// runtime is continuously frozen and unfrozen, because the lambda may be frozen before the "ACK" is received
+	// from the server. This can cause the keep-alive to timeout even though the connection is completely healthy.
+	// Therefore, keep-alives should be disabled in lambda and similar environments.
 	WithKeepAlivePermitWithoutCalls(keepAlivePermitWithoutCalls bool) GrpcConfiguration
 
 	// Returns number of milliseconds the client will wait for a response from a keepalive or ping.
+	GetKeepAliveTimeout() time.Duration
+
+	// WithKeepAliveTime Copy constructor for overriding the keepalive timeout. After waiting for a duration of this time,
+	// if the keepalive ping sender does not receive the ping ack, it will close the transport.
 	//
 	// NOTE: keep-alives are very important for long-lived server environments where there may be periods of time
 	// when the connection is idle. However, they are very problematic for lambda environments where the lambda
 	// runtime is continuously frozen and unfrozen, because the lambda may be frozen before the "ACK" is received
 	// from the server. This can cause the keep-alive to timeout even though the connection is completely healthy.
 	// Therefore, keep-alives should be disabled in lambda and similar environments.
-	GetKeepAliveTimeout() time.Duration
-
-	// WithKeepAliveTimeout Copy constructor for overriding the keepalive timeout.
 	WithKeepAliveTimeout(keepAliveTimeout time.Duration) GrpcConfiguration
 
 	// Returns the interval at which to send the keepalive or ping.
+	GetKeepAliveTime() time.Duration
+
+	// WithKeepAliveTimeout Copy constructor for overriding the keepalive time.
+	// After a duration of this time the client/server pings its peer to see if the transport is still alive.
 	//
 	// NOTE: keep-alives are very important for long-lived server environments where there may be periods of time
 	// when the connection is idle. However, they are very problematic for lambda environments where the lambda
 	// runtime is continuously frozen and unfrozen, because the lambda may be frozen before the "ACK" is received
 	// from the server. This can cause the keep-alive to timeout even though the connection is completely healthy.
 	// Therefore, keep-alives should be disabled in lambda and similar environments.
-	GetKeepAliveTime() time.Duration
-
-	// WithKeepAliveTime Copy constructor for overriding the keepalive time.
 	WithKeepAliveTime(keepAliveTime time.Duration) GrpcConfiguration
 
 	// WithKeepAlivePermitWithoutCalls Copy constructor for overriding the keepalive permit without calls.
