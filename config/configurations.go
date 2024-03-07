414c5dfc3bbbc3cda683d88db1cfefe37d3fe8bc
anitarua <anita@momentohq.com>
2024-03-07 14:22:48 -0800

    better doc comments for prebuilt configs

--- config/configurations.go
+++ config/configurations.go
@@ -12,6 +12,7 @@ import (
 )
 
 // LaptopLatest provides defaults suitable for a medium-to-high-latency dev environment.
+//
 // Permissive timeouts, retries, and relaxed latency and throughput targets.
 // It enables keep-alive pings by default because they are very important for long-lived server
 // environments where there may be periods of time when the connection is idle.
@@ -34,10 +35,10 @@ func LaptopLatestWithLogger(loggerFactory logger.MomentoLoggerFactory) Configura
 }
 
 // InRegionLatest provides defaults suitable for an environment where your client is running in the same region as the Momento service.
+//
 // It has more aggressive timeouts and retry behavior than the Laptop config.
 // It enables keep-alive pings by default because they are very important for long-lived server
 // environments where there may be periods of time when the connection is idle.
-
 func InRegionLatest() Configuration {
 	return InRegionLatestWithLogger(momento_default_logger.NewDefaultMomentoLoggerFactory(momento_default_logger.INFO))
 }
@@ -58,12 +59,12 @@ func InRegionLatestWithLogger(loggerFactory logger.MomentoLoggerFactory) Configu
 
 // LambdaLatest provides defaults suitable for an environment where your client is running in
 // a serverless environment like AWS Lambda.
+//
 // NOTE: keep-alives are very important for long-lived server environments where there may be periods of time
 // when the connection is idle. However, they are very problematic for lambda environments where the lambda
 // runtime is continuously frozen and unfrozen, because the lambda may be frozen before the "ACK" is received
 // from the server. This can cause the keep-alive to timeout even though the connection is completely healthy.
 // Therefore, keep-alives should be disabled in lambda and similar environments.
-
 func LambdaLatest() Configuration {
 	return LambdaLatestWithLogger(momento_default_logger.NewDefaultMomentoLoggerFactory(momento_default_logger.INFO))
 }
