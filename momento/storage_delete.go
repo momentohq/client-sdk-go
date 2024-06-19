2a3e82aa6dac0f052e4ae00bdd380f84a049f72a
anitarua <anita@momentohq.com>
2024-06-19 14:31:21 -0700

    fix docstring in StorageDeleteRequest

--- momento/storage_delete.go
+++ momento/storage_delete.go
@@ -1,7 +1,7 @@
 package momento
 
 type StorageDeleteRequest struct {
-	// StoreName is the name of the store to delete.
+	// StoreName is the name of the store to delete the key from.
 	StoreName string
 	// Key is the key to delete from the store.
 	Key string
