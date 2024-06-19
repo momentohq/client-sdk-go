9c2ac34509f0b5f4fb61405f2a4082601159a854
anitarua <anita@momentohq.com>
2024-06-19 14:35:49 -0700

    remove NextToken getter from ListStoresResponse

--- responses/list_stores.go
+++ responses/list_stores.go
@@ -21,13 +21,6 @@ func NewListStoresSuccess(nextToken string, stores []StoreInfo) *ListStoresSucce
 	}
 }
 
-// NextToken Next Page Token returned by Simple Store Service along with the list of stores.
-// If nextToken is present, then this token must be provided in the next call to continue paginating through the list.
-// This is done by setting this value in ListStoresRequest.
-func (resp ListStoresSuccess) NextToken() string {
-	return resp.nextToken
-}
-
 // Stores Returns all stores.
 func (resp ListStoresSuccess) Stores() []StoreInfo {
 	return resp.stores
