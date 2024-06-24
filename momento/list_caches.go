91d62de3e8de6e01288d4a5fa96130f989016246
Pete Gautier <pete@momentohq.com>
2024-06-24 09:42:37 -0700

    fix: reintroduce NextToken to list caches request

--- momento/list_caches.go
+++ momento/list_caches.go
@@ -1,3 +1,6 @@
 package momento
 
-type ListCachesRequest struct{}
+type ListCachesRequest struct {
+	// Token to continue paginating through the list. It's used to handle large paginated lists.
+	NextToken string
+}
