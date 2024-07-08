710de91919cf63a6d9299fefe49cbd64b4993aae
Pete Gautier <pete@momentohq.com>
2024-07-08 09:43:04 -0700

    chore: remove NextToken from ListCachesRequest

--- momento/list_caches.go
+++ momento/list_caches.go
@@ -1,6 +1,3 @@
 package momento
 
-type ListCachesRequest struct {
-	// Token to continue paginating through the list. It's used to handle large paginated lists.
-	NextToken string
-}
+type ListCachesRequest struct{}
