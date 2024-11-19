420aa446dd427a92529f8ab9e1740dcd4f690664
Michael Landis <michael@momentohq.com>
2024-11-19 13:26:39 -0800

    feat: document all rpc's and if they are idempotent or not
    Based on the current cache service proto, we add in all RPCs. For
    maintainability, we arrange them in the same order as in the proto
    file. For the RPCs that are new relative to when this was last
    updated, add a comment and mark whether they are idempotent or not.

--- internal/retry/eligibility_strategy.go
+++ internal/retry/eligibility_strategy.go
@@ -15,32 +15,60 @@ var retryableStatusCodes = map[codes.Code]bool{
 }
 
 var retryableRequestMethods = map[string]bool{
-	"/cache_client.Scs/Set":    true,
 	"/cache_client.Scs/Get":    true,
+	// NEW and idempotent "/cache_client.Scs/GetBatch"
+	"/cache_client.Scs/Set":    true,
+	// NEW and idempotent "/cache_client.Scs/SetBatch"
+	// NEW and _NOT_ idempotent "/cache_client.Scs/SetIf"
+
+	// deprecated and not idempotent "/cache_client.Scs/SetIfNotExists"
 	"/cache_client.Scs/Delete": true,
+	// NEW and idempotent "/cache_client.Scs/KeysExist"
 	// not idempotent "/cache_client.Scs/Increment"
-	"/cache_client.Scs/DictionarySet": true,
-	// not idempotent: "/cache_client.Scs/DictionaryIncrement",
+	// NEW and server-idempotent but return values can be different "/cache_client.Scs/UpdateTtl"
+	// NEW and idempotent "/cache_client.Scs/ItemGetTtl"
+	// NEW and idempotent "/cache_client.Scs/ItemGetType"
+
 	"/cache_client.Scs/DictionaryGet":    true,
 	"/cache_client.Scs/DictionaryFetch":  true,
+	"/cache_client.Scs/DictionarySet": true,
+	// not idempotent: "/cache_client.Scs/DictionaryIncrement",
 	"/cache_client.Scs/DictionaryDelete": true,
+	// NEW and idempotent "/cache_client.Scs/DictionaryLength"
+
+	"/cache_client.Scs/SetFetch":         true,
+	// NEW and idempotent "/cache_client.Scs/SetSample"
 	"/cache_client.Scs/SetUnion":         true,
 	"/cache_client.Scs/SetDifference":    true,
-	"/cache_client.Scs/SetFetch":         true,
-	// not idempotent: "/cache_client.Scs/SetIfNotExists"
+	// NEW and idempotent "/cache_client.Scs/SetContains"
+	// NEW and idempotent "/cache_client.Scs/SetLength"
+	// NEW and _NOT_ idempotent "/cache_client.Scs/SetPop"
+
 	// not idempotent: "/cache_client.Scs/ListPushFront",
 	// not idempotent: "/cache_client.Scs/ListPushBack",
 	// not idempotent: "/cache_client.Scs/ListPopFront",
 	// not idempotent: "/cache_client.Scs/ListPopBack",
-	"/cache_client.Scs/ListFetch": true,
+	// NEW, not used, and unknown "/cache_client.Scs/ListErase",
 	// Warning: in the future, this may not be idempotent
 	// Currently it supports removing all occurrences of a value.
 	// In the future, we may also add "the first/last N occurrences of a value".
 	// In the latter case it is not idempotent.
 	"/cache_client.Scs/ListRemove": true,
+	"/cache_client.Scs/ListFetch": true,
 	"/cache_client.Scs/ListLength": true,
 	// not idempotent: "/cache_client.Scs/ListConcatenateFront",
 	// not idempotent: "/cache_client.Scs/ListConcatenateBack"
+	// NEW and _NOT_ idempotent "/cache_client.Scs/ListRetain"
+
+	// NEW and idempotent "/cache_client.Scs/SortedSetPut"
+	// NEW and idempotent "/cache_client.Scs/SortedSetFetch"
+	// NEW and idempotent "/cache_client.Scs/SortedSetGetScore"
+	// NEW and idempotent "/cache_client.Scs/SortedSetRemove"
+	// NEW and _NOT_ idempotent "/cache_client.Scs/SortedSetIncrement"
+	// NEW and idempotent "/cache_client.Scs/SortedSetGetRank"
+	// NEW and idempotent "/cache_client.Scs/SortedSetLength"
+	// NEW and idempotent "/cache_client.Scs/SortedSetLengthByScore"
+
 	"/cache_client.pubsub.Pubsub/Subscribe": true,
 }
 
