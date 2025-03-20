480ca591cf4db4137636979c2ebdc885a5985c0f
anitarua <anita@momentohq.com>
2025-03-20 10:11:14 -0700

    feat: add response objects for new set if hash apis

---
+++ responses/set_if_absent_or_hash_equal.go
@@ -0,0 +1,39 @@
+package responses
+
+// SetIfAbsentOrHashEqualResponse is the base response type
+// for a set if absent or hash equal request.
+type SetIfAbsentOrHashEqualResponse interface {
+	isSetIfAbsentOrHashEqualResponse()
+}
+
+// SetIfAbsentOrHashEqualNotStored NotStored response to a
+// cache SetIfAbsentOrHashEqual api request.
+type SetIfAbsentOrHashEqualNotStored struct{}
+
+func (SetIfAbsentOrHashEqualNotStored) isSetIfAbsentOrHashEqualResponse() {}
+
+// SetIfAbsentOrHashEqualStored Stored response to a
+// cache SetIfAbsentOrHashEqual api request.
+type SetIfAbsentOrHashEqualStored struct {
+	hash []byte
+}
+
+func (SetIfAbsentOrHashEqualStored) isSetIfAbsentOrHashEqualResponse() {}
+
+// HashString Returns hash of value stored in cache as string
+// for SetIfAbsentOrHashEqualStored responses.
+func (resp SetIfAbsentOrHashEqualStored) HashString() string {
+	return string(resp.hash)
+}
+
+// HashByte Returns hash of value stored in cache as bytes
+// for SetIfAbsentOrHashEqualStored responses.
+func (resp SetIfAbsentOrHashEqualStored) HashByte() []byte {
+	return resp.hash
+}
+
+// NewSetIfAbsentOrHashEqualStored returns a new
+// SetIfAbsentOrHashEqualStored containing the supplied value.
+func NewSetIfAbsentOrHashEqualStored(hash []byte) *SetIfAbsentOrHashEqualStored {
+	return &SetIfAbsentOrHashEqualStored{hash: hash}
+}
---
+++ responses/set_if_absent_or_hash_not_equal.go
@@ -0,0 +1,39 @@
+package responses
+
+// SetIfAbsentOrHashNotEqualResponse is the base response type
+// for a set if absent or hash not equal request.
+type SetIfAbsentOrHashNotEqualResponse interface {
+	isSetIfAbsentOrHashNotEqualResponse()
+}
+
+// SetIfAbsentOrHashNotEqualNotStored NotStored response to a
+// cache SetIfAbsentOrHashNotEqual api request.
+type SetIfAbsentOrHashNotEqualNotStored struct{}
+
+func (SetIfAbsentOrHashNotEqualNotStored) isSetIfAbsentOrHashNotEqualResponse() {}
+
+// SetIfAbsentOrHashNotEqualStored Stored response to a
+// cache SetIfAbsentOrHashNotEqual api request.
+type SetIfAbsentOrHashNotEqualStored struct {
+	hash []byte
+}
+
+func (SetIfAbsentOrHashNotEqualStored) isSetIfAbsentOrHashNotEqualResponse() {}
+
+// HashString Returns hash of value stored in cache as string
+// for SetIfAbsentOrHashNotEqualStored responses.
+func (resp SetIfAbsentOrHashNotEqualStored) HashString() string {
+	return string(resp.hash)
+}
+
+// HashByte Returns hash of value stored in cache as bytes
+// for SetIfAbsentOrHashNotEqualStored responses.
+func (resp SetIfAbsentOrHashNotEqualStored) HashByte() []byte {
+	return resp.hash
+}
+
+// NewSetIfAbsentOrHashNotEqualStored returns a new
+// SetIfAbsentOrHashNotEqualStored containing the supplied value.
+func NewSetIfAbsentOrHashNotEqualStored(hash []byte) *SetIfAbsentOrHashNotEqualStored {
+	return &SetIfAbsentOrHashNotEqualStored{hash: hash}
+}
