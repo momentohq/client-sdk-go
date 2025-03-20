8ce96de72d80930009ade17b94e782c64877aa70
anitarua <anita@momentohq.com>
2025-03-20 10:11:14 -0700

    feat: add response objects for new set if hash apis

---
+++ responses/set_if_present_and_hash_not_equal.go
@@ -0,0 +1,39 @@
+package responses
+
+// SetIfPresentAndHashNotEqualResponse is the base response type
+// for a set if present and hash not equal request.
+type SetIfPresentAndHashNotEqualResponse interface {
+	isSetIfPresentAndHashNotEqualResponse()
+}
+
+// SetIfPresentAndHashNotEqualNotStored NotStored response to a
+// cache SetIfPresentAndHashNotEqual api request.
+type SetIfPresentAndHashNotEqualNotStored struct{}
+
+func (SetIfPresentAndHashNotEqualNotStored) isSetIfPresentAndHashNotEqualResponse() {}
+
+// SetIfPresentAndHashNotEqualStored Stored response to a
+// cache SetIfPresentAndHashNotEqual api request.
+type SetIfPresentAndHashNotEqualStored struct {
+	hash []byte
+}
+
+func (SetIfPresentAndHashNotEqualStored) isSetIfPresentAndHashNotEqualResponse() {}
+
+// HashString Returns hash of value stored in cache as string for
+// SetIfPresentAndHashNotEqualStored responses.
+func (resp SetIfPresentAndHashNotEqualStored) HashString() string {
+	return string(resp.hash)
+}
+
+// HashByte Returns hash of value stored in cache as bytes for
+// SetIfPresentAndHashNotEqualStored responses.
+func (resp SetIfPresentAndHashNotEqualStored) HashByte() []byte {
+	return resp.hash
+}
+
+// NewSetIfPresentAndHashNotEqualStored returns a new
+// SetIfPresentAndHashNotEqualStored containing the supplied value.
+func NewSetIfPresentAndHashNotEqualStored(hash []byte) *SetIfPresentAndHashNotEqualStored {
+	return &SetIfPresentAndHashNotEqualStored{hash: hash}
+}
