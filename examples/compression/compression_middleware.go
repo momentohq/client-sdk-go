34f0b5af1d1cedfbf5b069bf44c303d4cef17443
anitarua <anita@momentohq.com>
2025-04-07 12:23:28 -0700

    add compress and decompress helper functions

--- examples/compression/compression_middleware.go
+++ examples/compression/compression_middleware.go
@@ -37,63 +37,57 @@ func NewCompressionMiddlewareRequestHandler(rh middleware.RequestHandler, encode
 	return &compressionMiddlewareRequestHandler{rh, encoder, decoder}
 }
 
+func (rh *compressionMiddlewareRequestHandler) compress(requestType string, rawData []byte) []byte {
+	compressed := rh.encoder.EncodeAll(rawData, nil)
+	rh.GetLogger().Info("Compressed request %s: %d bytes -> %d bytes", requestType, len(rawData), len(compressed))
+	return compressed
+}
+
+func (rh *compressionMiddlewareRequestHandler) decompress(responseType string, rawData []byte) ([]byte, error) {
+	decompressed, err := rh.decoder.DecodeAll(rawData, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decompress response: %v", err)
+	}
+	rh.GetLogger().Info("Decompressed response %s: %d bytes -> %d bytes", responseType, len(rawData), len(decompressed))
+	return decompressed, nil
+}
+
+// Compress on write requests
 func (rh *compressionMiddlewareRequestHandler) OnRequest(req interface{}) (interface{}, error) {
-	// Compress on writes
+	// We still need to use a switch statement to be able to access the request objects
+	// as the specific request types in order to access the Value field.
 	switch r := req.(type) {
 	case *momento.SetRequest:
-		rh.GetLogger().Info("Setting key: %s, value: %s", r.Key, r.Value)
-		rawData := r.Value
-		compressed := rh.encoder.EncodeAll([]byte(fmt.Sprintf("%v", rawData)), nil)
-		rh.GetLogger().Info("Compressed request %T: %d bytes -> %d bytes", req, len(fmt.Sprintf("%v", rawData)), len(compressed))
-		return &momento.SetRequest{
-			CacheName: r.CacheName,
-			Key:       r.Key,
-			Value:     momento.String(compressed),
-		}, nil
+		compressed := rh.compress("SetRequest", []byte(fmt.Sprintf("%v", r.Value)))
+		r.Value = momento.Bytes(compressed)
+		return r, nil
 	case *momento.SetIfAbsentOrHashEqualRequest:
-		rh.GetLogger().Info("Setting key: %s, value: %s", r.Key, r.Value)
-		rawData := r.Value
-		compressed := rh.encoder.EncodeAll([]byte(fmt.Sprintf("%v", rawData)), nil)
-		rh.GetLogger().Info("Compressed request %T: %d bytes -> %d bytes", req, len(fmt.Sprintf("%v", rawData)), len(compressed))
-		return &momento.SetIfAbsentOrHashEqualRequest{
-			CacheName: r.CacheName,
-			Key:       r.Key,
-			Value:     momento.String(compressed),
-			HashEqual: r.HashEqual,
-		}, nil
+		compressed := rh.compress("SetIfAbsentOrHashEqualRequest", []byte(fmt.Sprintf("%v", r.Value)))
+		r.Value = momento.Bytes(compressed)
+		return r, nil
 	default:
 		rh.GetLogger().Info("No action for OnRequest type: %T", req)
 		return req, nil
 	}
 }
 
+// Decompress on read responses
 func (rh *compressionMiddlewareRequestHandler) OnResponse(resp interface{}) (interface{}, error) {
-	// Decompress on reads
+	// We still need to use a switch statement to be able to access the response objects
+	// as the specific response types in order to access the Value field.
 	switch r := resp.(type) {
 	case *responses.GetHit:
-		rawData := r.ValueByte()
-		decompressed, err := rh.decoder.DecodeAll(rawData, nil)
+		decompressed, err := rh.decompress("GetHit", r.ValueByte())
 		if err != nil {
 			return nil, fmt.Errorf("failed to decompress response: %v", err)
 		}
-		rh.GetLogger().Info(
-			"Decompressed response %T: %d bytes -> %d bytes",
-			resp, len(rawData), len(decompressed),
-		)
-		newGetResponse := responses.NewGetHit(decompressed)
-		return newGetResponse, nil
+		return responses.NewGetHit(decompressed), nil
 	case *responses.GetWithHashHit:
-		rawData := r.ValueByte()
-		decompressed, err := rh.decoder.DecodeAll(rawData, nil)
+		decompressed, err := rh.decompress("GetWithHashHit", r.ValueByte())
 		if err != nil {
 			return nil, fmt.Errorf("failed to decompress response: %v", err)
 		}
-		rh.GetLogger().Info(
-			"Decompressed response %T: %d bytes -> %d bytes",
-			resp, len(rawData), len(decompressed),
-		)
-		newGetWithHashResponse := responses.NewGetWithHashHit(decompressed, r.HashByte())
-		return newGetWithHashResponse, nil
+		return responses.NewGetWithHashHit(decompressed, r.HashByte()), nil
 	default:
 		rh.GetLogger().Info("No action for OnResponse type: %T", resp)
 		return resp, nil
